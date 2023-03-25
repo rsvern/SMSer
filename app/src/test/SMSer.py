6327d9865d267a00f5b0aa91cca0c1e885df9f81
Vern <rsvern@gmail.com>
2023-03-25 17:22:50 -0500

    Fix typo and make final \r\n read optional in SMSer.py test script

--- app/src/test/SMSer.py
+++ app/src/test/SMSer.py
@@ -1,11 +1,11 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 # Send SMS via SMSer using telnetlib.
 
 import telnetlib
 import sys
 import argparse
 
-SMSER_IP = '192.168.1.20` # '192.168.42.129'
+SMSER_IP = '192.168.1.20' # '192.168.42.129'
 SMSER_PORT = 8484
 
 parser = argparse.ArgumentParser()
@@ -22,17 +22,22 @@ tn = telnetlib.Telnet(SMSER_IP, SMSER_PORT)
 
 r = tn.read_until('\r\n', 10)
 if r != 'Hello from SMSer\r\n':
+  print('Error: no hello from SMSer')
   sys.exit(1)
 
 tn.write(args.phone + '\r\n')
 r = tn.read_until('\r\n', 10)
 if r != 'Address OK\r\n':
+  print('Error: phone number not OK')
   sys.exit(2)
 
 tn.write('%s\r\n' % (msg))
 tn.write('.\r\n')
 
-r = tn.read_until('\r\n', 10)
+try:
+  r = tn.read_until('\r\n', 10)
+except:
+  pass
 
 tn.close()
 sys.exit(0)
